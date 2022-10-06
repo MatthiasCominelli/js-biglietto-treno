c95692a3fb4badb35ea0682b8b1b70178b76eeb6
MatthiasCominelli <matticomi12@gmail.com>
2022-10-06 15:24:52 +0200

    biglietto migliorato

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-
+const utente = document.getElementById("nome-utente");
 const prezzototale = document.getElementById("prezzo-tratta");
-const nomeutente = prompt('Come ti chiami? (nome e cognome)')
+const nomeutente = prompt('Come ti chiami? (nome e cognome)');
 const kmdapercorrere = prompt('Quanti chilometri devi percorrere?');
 // console.log(kmdapercorrere);
 const etapasseggero = prompt('Quanti anni hai?');
@@ -10,16 +10,19 @@ let prezzotratta = kmdapercorrere * 0.21;
 
 if(etapasseggero < 18){
     let prezzoscontato20 = parseFloat ((prezzotratta * 0.8).toFixed(2));
-    console.log(prezzoscontato20);  
-    prezzototale.innerHTML = prezzoscontato20; 
+    console.log(prezzoscontato20); 
+    utente.innerHTML = nomeutente;
+    prezzototale.innerHTML = prezzoscontato20 + '€'; 
     
 }else if(etapasseggero > 65){
     let prezzoscontato40 = Number((prezzotratta * 0.6).toFixed(2));
     console.log(prezzoscontato40);
-    prezzototale.innerHTML = prezzoscontato40;
+    utente.innerHTML = nomeutente;
+    prezzototale.innerHTML = prezzoscontato40 + '€';
 }else{
     console.log(prezzotratta);
-    prezzototale.innerHTML = prezzotratta;
+    utente.innerHTML = nomeutente;
+    prezzototale.innerHTML = prezzotratta + '€';
 }
 
 
